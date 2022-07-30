c96206dc601267d2548e1d61e26af6226327bd03
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:15:29 -0300

    (JS) 15 - Operador lógico OR

---
+++ 15 - operador logico OR/js/source.js
@@ -0,0 +1,9 @@
+// operador logico OR (||)
+const atriz = 'Amanda Seyfried'
+const artista = 'MAMAMOO'
+
+if (atriz === 'Amanda Seyfried' || artista === 'MAMAMOO'){
+    console.log("São estrelas! podem entrar")
+} else {
+    console.log("Acesso não permitido")
+}
