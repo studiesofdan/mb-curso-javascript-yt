90a92ff4e61ef2519a3d1b4ce26b6c9e03321702
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:41:21 -0300

    (JS) 36 - Removendo elementos (removeChild)

---
+++ 36 - Removendo elementos (removeChild)/js/source.js
@@ -0,0 +1,8 @@
+// Removendo elementos
+let container = document.querySelector("#container")
+let p = document.querySelector("#container p")
+container.removeChild(p)
+
+// remover o elemento
+let subtitle = document.querySelector(".subtitle")
+subtitle.remove()
