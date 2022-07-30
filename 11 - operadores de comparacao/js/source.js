b1a83d968361332cc55d53457412bf5702aa4057
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:13:32 -0300

    (JS) 11 - Operadores de comparação

---
+++ 11 - operadores de comparacao/js/source.js
@@ -0,0 +1,21 @@
+// Operadores de comparação
+let numeroFavorito = 13
+let nomeMusicaDaTaylor = 22
+
+// Menor
+numeroFavorito < nomeMusicaDaTaylor
+
+// Maior
+numeroFavorito > nomeMusicaDaTaylor
+
+// Menor ou igual
+numeroFavorito <= nomeMusicaDaTaylor
+
+// Maior ou igual
+numeroFavorito >= nomeMusicaDaTaylor
+
+// Diferente
+numeroFavorito != nomeMusicaDaTaylor
+
+// Igual
+numeroFavorito == nomeMusicaDaTaylor
