bdad84a07f323f0aea677ee56f658aa659c07497
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:29:19 -0300

    (JS) 25 - Métodos numericos (parseInt, parseFloat)

---
+++ 25 - metodos numericos (parseInt, parseFloat)/js/source.js
@@ -0,0 +1,25 @@
+// Métodos numéricos
+// parseInt
+console.log("parseInt")
+console.log(parseInt(13.123))
+console.log(Number.parseInt(13.123))
+
+// parseFloat
+console.log("parseFloat")
+console.log(parseFloat(21.212))
+console.log(Number.parseFloat(21.212))
+
+// toFixed
+console.log("toFixed")
+console.log(30.1234567.toFixed(1))
+
+// isNaN
+console.log("isNaN")
+console.log(isNaN("pass the dutchie"))
+console.log(isNaN(13))
+console.log(isNaN("21"))
+
+// MAX_VALUE e MIN_VALUE
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(2.5352642837472364623748e+308)
