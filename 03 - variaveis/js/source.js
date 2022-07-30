dc9b1ad1d2f696de0b9af8d8322695b18e0ce783
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:07:20 -0300

    (JS) 03 - Variáveis

---
+++ 03 - variaveis/js/source.js
@@ -0,0 +1,26 @@
+// Tipos de variáveis
+// String
+let nome = 'Xanis Xanin'
+console.log(nome)
+
+// Numero
+let numero = 13
+console.log(numero)
+
+// undefined
+let operatingSystem
+operatingSystem = linux
+console.log(operatingSystem)
+
+// Escopo
+// global
+var global = "Variável global"
+console.log(global)
+
+// local
+let local = "Variável local"
+console.log(global)
+
+// reservada
+const reservada = "Variável reservada"
+console.log(reservada)
