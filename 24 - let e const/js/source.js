c85b95fff8ccd10429a9ebc341718a59c9012262
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:28:36 -0300

    (JS) 24 - Let e Const

---
+++ 24 - let e const/js/source.js
@@ -0,0 +1,13 @@
+// let e const
+// let
+let cor = 'azul'
+cor = 'vermelho'
+console.log(cor)
+
+// const
+const outraCor = 'amarelo'
+console.log(outraCor)
+
+// novo valor não pode ser atribuído ao const
+outraCor = 'verde'
+
