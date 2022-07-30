5d5f69a89ea8df09b2c13b617eadf1e1a9b38840
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:15:09 -0300

    (JS) 14 - Operador lógico AND

---
+++ 14 - operador logico AND/js/source.js
@@ -0,0 +1,9 @@
+// operador logico AND (&&)
+const atriz = 'Scarlett Johansson'
+const artista = 'Taylor Swift'
+
+if (atriz === 'Scarlett Johansson' && artista === 'Taylor Swift'){
+    console.log("São estrelas! podem entrar")
+} else {
+    console.log("Acesso não permitido")
+}
