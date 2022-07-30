456ac05ab9828d2d085e7d06c6934e8f3925b03b
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:08:41 -0300

    (JS) 06 - Boolean

---
+++ 06 - boolean/js/source.js
@@ -0,0 +1,13 @@
+// Boolean
+// True
+const verdadeiro = true;
+
+// False
+const falso = false;
+
+// Resultado
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+console.log(falso);
+console.log(typeof falso);
