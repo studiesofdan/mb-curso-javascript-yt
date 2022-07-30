438e1a34afd00b6fbab84addc889c2ab66361d6c
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:32:45 -0300

    (JS) 27 - Funções de string (parte 2)

---
+++ 27 - funções de string (parte 2)/js/source.js
@@ -0,0 +1,20 @@
+let nome = 'Selena Gomez'
+
+// toLowerCase
+console.log('toLowerCase: ', nome.toLowerCase())
+
+// toUpperCase
+console.log('toUpperCase: ', nome.toUpperCase())
+
+// trim
+let palavra = '      Som       ';
+let palavraTrim = palavra.trim();
+
+console.log('Sem trim: ', palavra)
+console.log('Com trim: ', palavraTrim)
+
+// split
+console.log(nome.split(" "))
+
+// lastIndexOf
+console.log(nome.lastIndexOf("z"))
