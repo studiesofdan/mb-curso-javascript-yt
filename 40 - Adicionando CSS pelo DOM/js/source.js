fe4fcdf05ad39b520a87f0699b4264051d55aae8
Danilo Santos <artededan@protonmail.ch>
2022-07-30 16:24:42 -0300

    (JS) 40 - Adicionando CSS pelo DOM

---
+++ 40 - Adicionando CSS pelo DOM/js/source.js
@@ -0,0 +1,11 @@
+// Adicionando CSS no DOM
+// Seleciona o elemento
+let elemento = document.querySelector("#title")
+
+// atribui a cor
+title.style.color = "red"
+title.style.backgroundColor = "yellow"
+
+// adicionar vários estilos
+let subtitle = document.querySelector("#subtitle")
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px"
