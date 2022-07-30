ea5ecfccd496c00e1fbc71cf8552cd4a86a2bda7
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:07:37 -0300

    (JS) 04 - Tipos de dados

---
+++ 04 - tipos de dados/js/source.js
@@ -0,0 +1,24 @@
+// Tipos de dados
+// numero
+let numero = 7
+console.log(numero)
+console.log(typeof numero)
+
+// float
+let float = 13.7
+console.log(float)
+console.log(typeof float)
+
+// string
+let letraComNumero = '737'
+console.log(letraComNumero)
+console.log(typeof letraComNumero)
+
+// outros
+// Not a Number
+console.log(NaN)
+console.log(typeof NaN)
+
+// infinity
+console.log(typeof + Infinity)
+console.log(typeof - Infinity)
