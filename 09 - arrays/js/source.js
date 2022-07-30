7b8113035065dcc6fd409a041354e0e4bad57036
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:12:55 -0300

    (JS) 09 - Arrays

---
+++ 09 - arrays/js/source.js
@@ -0,0 +1,17 @@
+// Arrays
+let arr = [7, "Dan", false, {teste: 1, teste: 2}]
+console.log(arr)
+
+let harry = ["h", "a", "r", "r", "y"]
+console.log(harry)
+
+let arrTwo = [5, 4, 3, 2, 1]
+console.log(arrTwo)
+console.log(typeof arrTwo)
+
+// acessando um array
+console.log(arr[1])
+
+// adicionando um array
+arr[9] = "Taylor"
+console.log(arr)
