44361b03cfd420bc65e0eaad7018707c9809290b
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:12:38 -0300

    (JS) 08 - Object

---
+++ 08 - object/js/source.js
@@ -0,0 +1,12 @@
+// Object
+const object = {
+    nome: "Danilo Santos",
+    idade: 27,
+    profissao: "Desenvolvedor Java"
+}
+
+console.log(object)
+console.log(typeof object)
+
+// acessando um objeto
+console.log(object.nome)
