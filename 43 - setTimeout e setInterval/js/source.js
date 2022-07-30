fa3f92355967260f19421e63f85cd4002aaf42d0
Danilo Santos <artededan@protonmail.ch>
2022-07-30 16:30:27 -0300

    (JS) 43 - SetTimeout e setInterval

---
+++ 43 - setTimeout e setInterval/js/source.js
@@ -0,0 +1,14 @@
+// setTimeout e setInterval
+// setTimeout
+console.log("Antes do timeout")
+
+setTimeout(function() {
+    console.log("Testando o timeout")
+}, 2000)
+
+// setInterval
+console.log("Antes do setInterval")
+
+setInterval(function() {
+    console.log("Testando o setInterval")
+}, 1000)
