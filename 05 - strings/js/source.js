9975cc693e36468aa39cc9d650406930694c6422
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:07:51 -0300

    (JS) 05 - Strings

---
+++ 05 - strings/js/source.js
@@ -0,0 +1,13 @@
+// Strings
+// Aspas simples
+const aspasSimples = 'Tuts';
+
+// Aspas duplas
+const aspasDuplas = "Tuts";
+
+// Concatenação
+const nome = "Danilo"
+const sobrenome = "Santos"
+const nomeCompleto = nome + " " + sobrenome
+console.log(nomeCompleto)
+console.log(typeof nomeCompleto)
