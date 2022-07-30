8215043210d2ab0b9d44293ee2e09f15e2cdff8d
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:36:18 -0300

    (JS) 32 - Acessando elementos através do DOM

---
+++ 32 - acessando elementos através do DOM/js/source.js
@@ -0,0 +1,15 @@
+// Document Object Model
+// tag
+let titulo = document.getElementsByTagName('h1')[0]
+console.log(titulo)
+
+let lista = document.getElementsByTagName('li')
+console.log(lista[2])
+
+// id
+let paragrafo = document.getElementById('paragrafo')
+console.log(paragrafo)
+
+// class
+let itensDaLista = document.getElementsByClassName('item')
+console.log(itensDaLista)
