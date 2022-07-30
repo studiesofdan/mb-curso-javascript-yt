2c7c830785294fd64e2b9fc0f11929a073983f18
Danilo Santos <artededan@protonmail.ch>
2022-07-30 16:31:58 -0300

    (JS) 45 - Eventos e onload

---
+++ 45 - Eventos e onload/js/source.js
@@ -0,0 +1,12 @@
+// Eventos e onload
+window.onload = function() {
+    console.log("Carregou o DOM")
+    
+    let tituloDois = document.querySelector("#title")
+    console.log(tituloDois)
+}
+
+console.log("Carregou o JS")
+
+let titulo = document.querySelector("#title")
+console.log(titulo)
