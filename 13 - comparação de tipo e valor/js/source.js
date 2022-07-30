5310246a5eb511322037a3432973664740dade8a
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:14:30 -0300

    (JS) 13 - Comparação de tipo e valor

---
+++ 13 - comparação de tipo e valor/js/source.js
@@ -0,0 +1,13 @@
+// comparação de tipo e valor
+// usado para garantir que o valor recebido é do mesmo tipo
+// ex: string é realmente string
+
+const numero = '7'
+
+if (numero === '7'){
+    console.log(true)
+}
+
+else if (numero !== 7){
+    console.log("O número não é do tipo number")
+}
