1f8753516a5be33ec1deaa368690b1f5728c296d
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:39:35 -0300

    (JS) 33 - querySelector e querySelectorAll

---
+++ 33 - querySelector e querySelectorAll/js/source.js
@@ -0,0 +1,12 @@
+// querySelector e querySelectorAll
+// exemplo de acesso com className
+let itensClasse = document.getElementsByClassName('item')
+console.log(itensClasse)
+
+// querySelectorAll (pegar vários elementos)
+let itensQuery = document.querySelectorAll('#segundaLista li')
+console.log(itensQuery)
+
+// querySelector (pegar um elemento)
+let itensQuery2 = document.querySelector('ul')
+console.log(itensQuery2)
