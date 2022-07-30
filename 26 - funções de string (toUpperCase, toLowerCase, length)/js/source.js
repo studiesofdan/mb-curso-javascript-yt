81fb0a41bf53de1ae807c454fba477bcd9f1a5e2
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:29:55 -0300

    (JS) 26 - Funções de string (toUpperCase, toLowerCase, length)

---
+++ 26 - funções de string (toUpperCase, toLowerCase, length)/js/source.js
@@ -0,0 +1,17 @@
+// length
+let nome = 'Dan'
+console.log(nome.length)
+
+// indexOf
+console.log(nome[2])
+
+let frase = "Taylor Swift"
+console.log(frase.indexOf("Swift"))
+
+// slice
+let sobrenome = frase.slice(1, 2)
+console.log(sobrenome)
+
+// replace
+let novaFrase = frase.replace("Swift", "Alwyn")
+console.log(novaFrase)
