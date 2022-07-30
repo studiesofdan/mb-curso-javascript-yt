10f21a16dc4677123d6b6127e08601ec2d193320
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:26:36 -0300

    (JS) 17 - Estrutura de repetição while

---
+++ 17 - estrutura de repetição while/js/source.js
@@ -0,0 +1,9 @@
+// estrutura de repetição while
+const meow = "MIAU MIAU MIAU MEOW MEOW MAU MAU ME ME ME ME-OW"
+let numeroArtistasFestival = 0;
+
+while (numeroArtistasFestival <= 47){
+    console.log(meow[numeroArtistasFestival]);
+
+    numeroArtistasFestival++;
+}
