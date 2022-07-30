00470cc0aa8f67f89e1a1a0828b99d746398926d
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:26:53 -0300

    (JS) 18 - Operadores de atribuição

---
+++ 18 - operadores de atribuicao/js/source.js
@@ -0,0 +1,24 @@
+// operadores de atribuição
+// +=, -=, *=, /=
+
+let numeroUm = 2
+let numeroDois = 2
+
+// Sem operador de atribuição
+console.log("Sem operador de atribuição: ", numeroUm = numeroUm + numeroDois)
+
+// Atribuindo valores
+// Divisão
+console.log("Operador de atribuição: Divisão", numeroUm /= numeroDois)
+
+// Multiplicação
+console.log("Operador de atribuição: Multiplicação", numeroUm *= numeroDois)
+
+// Adição
+console.log("Operador de atribuição: Soma", numeroUm += numeroDois)
+
+// Subtração
+console.log("Operador de atribuição: Subtração", numeroUm -= numeroDois)
+
+
+
