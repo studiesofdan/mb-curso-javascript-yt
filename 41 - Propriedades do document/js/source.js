a4979482c83182a53ae0d1383d5f596e406203cc
Danilo Santos <artededan@protonmail.ch>
2022-07-30 16:25:04 -0300

    (JS) 41 - Propriedades do document

---
+++ 41 - Propriedades do document/js/source.js
@@ -0,0 +1,12 @@
+// Propriedades do document
+console.log(document.body)
+console.log(document.head)
+console.log(document.links[0])
+
+document.links[0].textContent = "Croissant"
+
+console.log(document.URL)
+console.log(document.title)
+
+document.title = "Aula 44"
+console.log(document.title)
