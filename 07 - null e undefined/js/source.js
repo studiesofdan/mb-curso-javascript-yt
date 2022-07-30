98ff5d54426e67f1cd1d2ae71fbb2232d2e4c9a0
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:08:58 -0300

    (JS) 07 - Null e undefined

---
+++ 07 - null e undefined/js/source.js
@@ -0,0 +1,13 @@
+// Null e undefined
+// True
+const nulo = null;
+
+// False
+const naoTemValor = undefined;
+
+// Resultado
+console.log(nulo);
+console.log(typeof nulo);
+
+console.log(naoTemValor);
+console.log(typeof naoTemValor);
