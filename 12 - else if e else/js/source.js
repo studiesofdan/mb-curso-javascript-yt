6cea7c80688c02eca9a9323d95b49f27df40a742
Danilo Santos <artededan@protonmail.ch>
2022-07-30 15:14:09 -0300

    (JS) 12 - Else if e else

---
+++ 12 - else if e else/js/source.js
@@ -0,0 +1,14 @@
+// Else if e else
+let nota = 9.0
+
+if (nota >= 7.0) {
+    console.log("Aprovado")
+} 
+
+else if (nota < 7.0) {
+    console.log("Reprovado")
+}
+
+else {
+    console.log("Por gentileza, inserir a nota para continuar")
+}
